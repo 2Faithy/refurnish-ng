b257e5ebedc65c9c2e1f6bc70671cdc2010c0769
Faith Onwuemeri <faithonwuemeri2008@gmail.com>
2025-07-22 07:50:35 +0100

    Ready for production deploy

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,12 @@
+// src/app/checkout/page.tsx
+
+import React from "react";
+
+export default function CheckoutPage() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      {/* Your page content */}
+    </div>
+  );
+}
